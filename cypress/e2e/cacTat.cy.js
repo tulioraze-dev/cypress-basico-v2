d00ff3b4ad0fe4bd17c3062497c756ffd715c8b1
tulioraze-dev <tuliorazevedo.dev@gmail.com>
2024-02-12 10:50:40 -0300

    Usando a biblioteca loadsh função time

--- cypress/e2e/cacTat.cy.js
+++ cypress/e2e/cacTat.cy.js
@@ -121,10 +121,12 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.contains('Talking About Testing').should('be.visible')
   })
-
-  it('testa a página da política de privacidade de forma independente', () => {
-    cy.visit('../src/privacy.html')
-
-    cy.contains('Talking About Testing').should('be.visible')
+  
+  Cypress._.times('5', () => {
+    it('testa a página da política de privacidade de forma independente', () => {
+      cy.visit('../src/privacy.html')
+  
+      cy.contains('Talking About Testing').should('be.visible')
+    })
   })
 })
